98538a71ba1190a3fed1042a0db1c4c47c0d807e
RatSauce <58752704+SpicyRatSauce@users.noreply.github.com>
2025-06-28 15:54:48 -0300

    Update and rename example.sh to final.sh

--- files/scripts/final.sh
+++ files/scripts/final.sh
@@ -5,6 +5,6 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-# Your code goes here.
-echo 'This is an example shell script'
-echo 'Scripts here will run during build if specified in recipe.yml'
+systemctl set-default graphical.target
+
+systemctl enable sddm.service
